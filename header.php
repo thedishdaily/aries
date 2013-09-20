cb8ab541694f64e67b8988553fc76a879c71ab45
Roger Chen <rogerthechen@gmail.com>
2013-09-20 00:11:07 +0000

    Header partially implemented

--- header.php
+++ header.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
-        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />        
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?>
-    </head>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_title(); ?></title>
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />        
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+</head>
+<header class="navbar navbar-inverse navbar-fixed-top aries-navbar">
+    <div class="container">
+        <div class="aries-navbar-logo"><img src="<? bloginfo('template_directory');?>/images/logo.png" /></div>
+        <div class="aries-navbar-items">
+            <ul>
+                <li>Item One</li>
+                <li>Item Two</li>
+            </ul>
+        </div>
+    </div>
+</header>
+<body>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
-<div id="main">
-<div id="content">
+<div class="container">
 <h1>Main Area</h1>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <h1><?php the_title(); ?></h1>
@@ -10,6 +9,5 @@
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 </div>
 </div>
-<div id="delimiter">
 </div>
 <?php get_footer(); ?>

3482d695e115bbb1419d564998a0b2b9624d11e1
Roger Chen <rogerthechen@gmail.com>
2014-01-12 18:23:42 +0000

    Fixed issue with featured image being scaled incorrectly

--- single-template.php
+++ single-template.php
@@ -15,7 +15,7 @@
 					<div class="aries-post-subtitle"><? the_subtitle(); ?></div>
 					<?php if (has_post_thumbnail( $post->ID ) ): ?>
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-					<div class="aries-post-image"><img src="<? echo $image[0]; ?>" /></div>
+					<div class="aries-post-image" style="background-image: url('<? echo $image[0]; ?>');"></div>
 					<?php endif; ?>
 					<div class="aries-post-content"><?php the_content(__('Read more'));?></div>
 					<div class="aries-post-readmore">
